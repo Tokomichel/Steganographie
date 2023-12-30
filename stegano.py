a0ff83f4cb93cec6600ccae1124de49c7b5744e5
Tokomichel <tokowouendeu@gmail.com>
2023-12-30 22:11:47 +0100

    merge opt in master

--- stegano.py
+++ stegano.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from numpy import asarray, array
 
+
 def hide(message: str, image_loc: str, result_image: str = "cache.png"):
     image = Image.open(image_loc)
     data = asarray(image)
@@ -14,14 +15,14 @@ def hide(message: str, image_loc: str, result_image: str = "cache.png"):
         while len(binary) < 8:
             binary = "0" + binary
         message_binaire += binary
-    
-    taille_binaire = bin(len(message_binaire))[2:] # on converti en binaire la taille du message
-    
+
+    taille_binaire = bin(len(message_binaire))[2:]  # on converti en binaire la taille du message
+
     # on determine la longueur en binaire de la taille.
     # on encode la taille sur 16 bits ainsi on aura environ 2ko d'espace
     while len(taille_binaire) < 16:
         taille_binaire = "0" + taille_binaire
-        
+
     print(len(message_binaire), taille_binaire)
     message_binaire = taille_binaire + message_binaire
     # on va parcourir notre tableau pour cacher le message dans chaque bits de chaque pixels
@@ -46,6 +47,7 @@ def hide(message: str, image_loc: str, result_image: str = "cache.png"):
 
     Image.fromarray(donnees).save(result_image)
 
+
 def read(image: str, encode_lenght: int = 16):
     # on cherche d'abord la taille du message a lire
     data = asarray(Image.open(image))
@@ -65,10 +67,10 @@ def read(image: str, encode_lenght: int = 16):
                 break
         if x >= encode_lenght:
             break
-    
+
     message_lenght = int(taille_message, 2)
     print(f"taille du message: {message_lenght}, taille message bin: {taille_message}")
-    
+
     tour = 0
     message = ""
     for i in range(len(data)):
@@ -82,43 +84,38 @@ def read(image: str, encode_lenght: int = 16):
             if tour >= (message_lenght + encode_lenght):
                 break
         if tour >= (message_lenght + encode_lenght):
-            break    
-    
-    # on converti le message en chaine de caractere
+            break
+
+            # on converti le message en chaine de caractere
     # sans oublier que chaque lettre etait encodee sur 8 bits
     clear_message = ""
     x = 0
     liste = split_by(message, 8)
-    
+
     for elt in liste:
         clear_message += chr(int(elt, 2))
-    
-    print(clear_message)    
+
+    print(clear_message)
+
 
 def split_by(chaine: str, lenght: int) -> list:
-
     x = 0
     lettre = ""
     liste = []
     clear_liste = []
     for elt in chaine:
         lettre += elt
-        
+
         if x < (lenght - 1):
-            x += 1 
+            x += 1
         else:
             liste.append(lettre)
             lettre = ""
             x = 0
-    
-    return liste
-    
 
-        
-          
+    return liste
+
+
 if __name__ == "__main__":
     # hide("Je fais un teste plus performant avec un texte plus grand voir si ca marche", "eren.png")
     read("cache.png")
-    
-    
-    
